03b2a55a50a3bafb9c44d52de74cfd3c7b072429
Vladislav <Yanchevsky>
2019-02-02 13:12:43 +0300

    Add select and datepicker

---
+++ src/components/basic/AppSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <select class="custom-select form-select" @change="handleChange">
+        <option :value="option" v-for="(option, index) in options" v-bind:key="index">{{option}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "select",
+  props: {
+      options: Array
+  },
+  methods: {
+    handleChange: function(event) {
+      this.$emit("input", event.target.value);
+    },
+  }
+};
+</script>
+
+<style>
+.form-select {
+	width: 200px !important;
+	margin-bottom: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <app-select v-model="task" class="inline" :options="tasks"></app-select>
+    <app-select v-model="progress" class="inline" :options="statuses"></app-select>
+    <div style="width: 320px; margin: auto">
+      <date-picker class="form-input center" :value="state.date"></date-picker>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AppSelect from "@/components/basic/AppSelect.vue";
+import DatePicker from "vuejs-datepicker";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+const tasks = ['Sleep', 'Do labs', 'Watch anime']
+const statuses = ['Not started', 'In progress', 'Finished']
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    AppSelect,
+    DatePicker
+  },
+  data() {
+    return {
+      tasks,
+      statuses,
+      task: 'Sleep',
+      progress: 'Not started',
+      state: {
+        date: new Date(2016, 9,  16)
+      }
+    }
   }
 };
 </script>
+
+<style>
+.inline {
+  display: inline;
+  margin-left: 10px;
+}
+
+.center {
+  text-align: inherit;
+}
+
+</style>
+
